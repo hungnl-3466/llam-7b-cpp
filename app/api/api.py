cf46da568316423864cf7175b55a4ef5026a3009
hungnl-3466 <nguyen.luong.hung@sun-asterisk.com>
2024-05-21 17:41:43 +0700

    Define API

--- app/api/api.py
+++ app/api/api.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import sys
+from pydantic import BaseModel
 
-app  = FastAPI()
+sys.path.insert(0, 'app')
+from infrastructure.llama_cpp_infer import llm_process
+
+app  = FastAPI()    
 
 origins = ["*"]
 app.add_middleware(
@@ -13,10 +18,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+class InputData(BaseModel):
+    input_text: str
+    
 @app.post("/get_input")
-async def get_input(input_text: str= Form()):
-    return JSONResponse(status_code=200, content={"input": input_text})
+async def get_input(data: InputData):   
+    input_text = data.input_text
+    print(input_text)
+    output = llm_process(input_text)
+    return JSONResponse(status_code=200, content={"llama-7b-cpp": output})
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
